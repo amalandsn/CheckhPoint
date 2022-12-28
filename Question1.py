a52e097e08cd606f25b4e936412804eda581f02d
amalandsn <86369471+amalandsn@users.noreply.github.com>
2022-12-28 15:46:17 +0000

    contenu changé

--- Question1.py
+++ Question1.py
@@ -1,1 +1,8 @@
-print("hello")
+# Question 1
+result = []  # Création d'une liste vide
+
+for i in range(2000, 3201):  
+    if i % 7 == 0 and i % 5 != 0: 
+        result.append(i)  # si ok ajout du nombre dans la liste
+
+print(result)  # affichage du résulta
