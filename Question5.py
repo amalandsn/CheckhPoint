277513bd3eb8d175a9936a549a3ea096ed98d15d
amalandsn <86369471+amalandsn@users.noreply.github.com>
2022-12-28 16:01:38 +0000

    avec le code

---
+++ Question5.py
@@ -0,0 +1,11 @@
+# Question 5
+import numpy as np
+
+# NumPy array
+arr = np.array([[0, 1], [2, 3], [4, 5]])
+
+# convertir le numpy array en list
+lst = arr.tolist()
+
+print("Array d'origine :", arr)
+print("Converti en list:", lst)
